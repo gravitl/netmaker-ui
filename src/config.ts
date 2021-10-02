df56c2053e8b112427927b5a6a98b63902c37a50
Håkan Persson <hakan.persson.93@gmail.com>
2021-10-02 22:33:03 +0200

    Use UI_VERSION from package.json

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,10 @@
+import { version } from '../package.json'
 // window.REACT_APP_BACKEND for container usage
 // if running locally or statically, change the 'http://localhost:8081' to desired backend URL and then build
 export const BACKEND_URL = (window as any).REACT_APP_BACKEND || 'http://192.168.0.152:8081'
 
 // == set UI version here ==
-export const UI_VERSION = "0.8.0"
+export const UI_VERSION = version
 
 // == local storage keys == 
 export const USER_KEY = "netmaker-user"
