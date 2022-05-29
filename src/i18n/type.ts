b679316f844a547de5a777016c725c52b403424b
FatherWolf <engleau@gmail.com>
2022-05-29 18:33:18 -0400

    buttons and some logic added

--- src/i18n/type.ts
+++ src/i18n/type.ts
@@ -1,6 +1,7 @@
 export interface LanguageResource {
   pro: {
     logs: string
+    
   }
   header: {
     login: string
--- src/route/logs/ServerLogs.tsx
+++ src/route/logs/ServerLogs.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography } from '@mui/material'
+import { Container, Grid, IconButton, Typography, InputAdornment, TextField, Tooltip } from '@mui/material'
 import React from 'react'
 import { useRouteMatch, Switch, Route } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
@@ -7,6 +7,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { serverSelectors } from '~store/selectors'
 import { LinearProgress } from '@mui/material'
 import { getServerLogs } from '~store/modules/server/actions'
+import { Search, Sync } from '@mui/icons-material'
 
 export const ServerLogs: React.FC = () => {
   const { path, url } = useRouteMatch()
@@ -15,6 +16,7 @@ export const ServerLogs: React.FC = () => {
   const serverLogs = useSelector(serverSelectors.getServerLogs)
   const [currentLogs, setCurrentLogs] = React.useState([] as string[])
   const dispatch = useDispatch()
+  const [filterLogs, setFilterLogs] = React.useState(serverLogs)
 
   useLinkBreadcrumb({
     link: url,
@@ -22,7 +24,7 @@ export const ServerLogs: React.FC = () => {
   })
 
   const refreshLogs = () => {
-      setCurrentLogs([])
+    setCurrentLogs([])
   }
 
   const titleStyle = {
@@ -30,13 +32,24 @@ export const ServerLogs: React.FC = () => {
   } as any
 
   React.useEffect(() => {
-      if (!!!currentLogs.length) {
-        dispatch(getServerLogs.request())
-      } 
-      if (currentLogs.length !== serverLogs.length || currentLogs !== serverLogs) {
-          setCurrentLogs(serverLogs)
-      }
+    if (!!!currentLogs.length) {
+      dispatch(getServerLogs.request())
+    }
+    if (currentLogs.length !== serverLogs.length || currentLogs !== serverLogs) {
+      setCurrentLogs(serverLogs)
+    }
   }, [dispatch, currentLogs, serverLogs])
+
+  const handleFilter = (event: { target: { value: string } }) => {
+    const { value } = event.target
+    const searchTerm = value.trim()
+    if (!!!searchTerm) {
+      setFilterLogs(serverLogs)
+    } else {
+      setFilterLogs(serverLogs.filter(log => log.includes(searchTerm)))
+    }
+  }
+
 
   return (
     <Container>
@@ -48,20 +61,43 @@ export const ServerLogs: React.FC = () => {
             justifyContent="center"
             alignItems="center"
           >
-            <Grid item xs={5}>
-              <div style={titleStyle}>
-                <Typography variant="h5">
-                  {t('pro.logs')}
-                </Typography>
-              </div>
+            <Grid item xs={4}>
+              <Grid container justifyContent='space-around' alignItems='center'>
+                <div style={titleStyle}>
+                  <Typography variant="h5">
+                    {t('pro.logs')}
+                  </Typography>
+                </div>
+              </Grid>
+              <Grid item xs={5}>
+                <Grid container justifyContent='space-around' alignItems='center'>
+                  <TextField
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position='start'>
+                          <Search />
+                        </InputAdornment>
+                      ),
+                    }}
+                    label={`${t('common.search')} ${t('pro.logs')}`}
+                    onChange={handleFilter} />
+
+                </Grid>
+                <Grid item xs={5}>
+                  <IconButton color='primary' onClick={refreshLogs}>
+                    <Sync />
+                  </IconButton>
+                </Grid>
+              </Grid>
+              </Grid>
             </Grid>
             <Grid item xs={12}>
-                {isFetching && <LinearProgress />}
-                {currentLogs.map(log => <Typography variant='body2'>
-                    {log}
-                </Typography>)}
+              {isFetching && <LinearProgress />}
+              {currentLogs.map(log => <Typography variant='body2'>
+                {log}
+              </Typography>)}
             </Grid>
-          </Grid>
+          
         </Route>
       </Switch>
     </Container>
