32a876e97b0bc27ab30cd2055a7552c57fcde8be
FatherWolf <engleau@gmail.com>
2022-06-07 10:18:18 -0400

    added filter selector and icon to nodes

--- src/route/nodes/Nodes.tsx
+++ src/route/nodes/Nodes.tsx
@@ -1,4 +1,11 @@
-import { Container, Grid, IconButton, InputAdornment, TextField, Tooltip } from '@mui/material'
+import {
+  Container,
+  Grid,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Tooltip,
+} from '@mui/material'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { nodeSelectors } from '~store/selectors'
@@ -8,25 +15,30 @@ import { NodeTable } from './components/NodeTable'
 import { NetworkSelect } from '../../components/NetworkSelect'
 import { Search, Sync } from '@mui/icons-material'
 import { NetworkNodes } from './netid/NetworkNodes'
+import { Tablefilter } from '../networks/components/Tablefilter'
 
 export const Nodes: React.FC = () => {
   const { path } = useRouteMatch()
   const { t } = useTranslation()
   const listOfNodes = useSelector(nodeSelectors.getNodes)
-  const [ filterNodes, setFilterNodes ] = React.useState(listOfNodes)
+  const [filterNodes, setFilterNodes] = React.useState(listOfNodes)
   const history = useHistory()
 
   const syncNodes = () => {
     history.push('/nodes')
   }
 
-  const handleFilter = (event: {target: {value: string}}) => {
+  const handleFilter = (event: { target: { value: string } }) => {
     const { value } = event.target
     const searchTerm = value.trim()
     if (!!!searchTerm) {
       setFilterNodes(listOfNodes)
     } else {
-      setFilterNodes(listOfNodes.filter(node => `${node.name}${node.address}${node.network}`.includes(searchTerm)))
+      setFilterNodes(
+        listOfNodes.filter((node) =>
+          `${node.name}${node.address}${node.network}`.includes(searchTerm)
+        )
+      )
     }
   }
 
@@ -35,25 +47,25 @@ export const Nodes: React.FC = () => {
       <Switch>
         <Route exact path={path}>
           <Grid container justifyContent="space-around" alignItems="center">
-            <Grid item xs={4}>
+            <Grid item xs={3}>
               <h2>{t('node.nodes')}</h2>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={7}>
               <Grid container justifyContent="space-around" alignItems="center">
                 <Grid item xs={4}>
-                  <TextField 
+                  <TextField
                     InputProps={{
                       startAdornment: (
-                        <InputAdornment position='start'>
+                        <InputAdornment position="start">
                           <Search />
                         </InputAdornment>
                       ),
                     }}
-                    label={`${t('common.search')} ${t('node.nodes')}`} 
-                    onChange={handleFilter} 
+                    label={`${t('common.search')} ${t('node.nodes')}`}
+                    onChange={handleFilter}
                   />
                 </Grid>
-                <Grid item xs={7}>
+                <Grid item xs={3}>
                   <NetworkSelect />
                 </Grid>
                 <Grid item xs={1}>
@@ -63,10 +75,19 @@ export const Nodes: React.FC = () => {
                     </IconButton>
                   </Tooltip>
                 </Grid>
+                <Grid item xs={3}>
+                  <Tablefilter />
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-          <NodeTable nodes={filterNodes.length && filterNodes.length < listOfNodes.length ? filterNodes : listOfNodes}/>
+          <NodeTable
+            nodes={
+              filterNodes.length && filterNodes.length < listOfNodes.length
+                ? filterNodes
+                : listOfNodes
+            }
+          />
         </Route>
 
         <Route path={`${path}/:netid`}>
