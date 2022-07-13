51e2f7ea4cfd5b2be4e9beac044258bfeb5ff38d
FatherWolf <engleau@gmail.com>
2022-07-13 17:03:08 -0400

    added actions, reducer,saga,selectors, and types for user groups

--- src/store/modules/pro/reducer.ts
+++ src/store/modules/pro/reducer.ts
@@ -4,61 +4,129 @@ import {
   getUserGroups,
   deleteUserGroup,
   createUserGroup,
+  getNetworkUsers,
+  deleteNetworkUser,
+  updateNetworkUser,
 } from './actions'
+import { NetworksUsersMap } from './types'
 
 export const reducer = createReducer({
   isProcessing: false as boolean,
   userGroups: [] as string[],
+  networkUsers: {} as NetworksUsersMap,
 })
-.handleAction(getUserGroups['success'], (state, payload) =>
-  produce(state, (draftState) => {
-    draftState.isProcessing = false
-    if (!!payload && !!payload.payload) {
+  .handleAction(getUserGroups['success'], (state, payload) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+      if (!!payload && !!payload.payload) {
+        draftState.userGroups = []
+        Object.keys(payload.payload).map((group) =>
+          draftState.userGroups.push(group)
+        )
+      }
+    })
+  )
+  .handleAction(getUserGroups['failure'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
       draftState.userGroups = []
-      Object.keys(payload.payload).map(group => draftState.userGroups.push(group))
-    }
-  })
-)
-.handleAction(getUserGroups['failure'], (state, _) =>
-  produce(state, (draftState) => {
-    draftState.isProcessing = false
-    draftState.userGroups = []
-  })
-)
-.handleAction(getUserGroups['request'], (state, _) =>
-  produce(state, (draftState) => {
-    draftState.isProcessing = true
-  })
-)
-.handleAction(deleteUserGroup['request'], (state, _) =>
-  produce(state, (draftState) => {
-    draftState.isProcessing = true
-  })
-)
-.handleAction(deleteUserGroup['failure'], (state, _) =>
-  produce(state, (draftState) => {
-    draftState.isProcessing = false
-  })
-)
-.handleAction(deleteUserGroup['success'], (state, payload) =>
-  produce(state, (draftState) => {
-    draftState.isProcessing = false
-    draftState.userGroups = draftState.userGroups.filter(group => group !== payload.payload.groupName)
-  })
-)
-.handleAction(createUserGroup['request'], (state, _) =>
-  produce(state, (draftState) => {
-    draftState.isProcessing = true
-  })
-)
-.handleAction(createUserGroup['failure'], (state, _) =>
-  produce(state, (draftState) => {
-    draftState.isProcessing = false
-  })
-)
-.handleAction(createUserGroup['success'], (state, payload) =>
-  produce(state, (draftState) => {
-    draftState.isProcessing = false
-    draftState.userGroups.push(payload.payload.groupName)
-  })
-)
+    })
+  )
+  .handleAction(getUserGroups['request'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = true
+    })
+  )
+  .handleAction(deleteUserGroup['request'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = true
+    })
+  )
+  .handleAction(deleteUserGroup['failure'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+    })
+  )
+  .handleAction(deleteUserGroup['success'], (state, payload) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+      draftState.userGroups = draftState.userGroups.filter(
+        (group) => group !== payload.payload.groupName
+      )
+    })
+  )
+  .handleAction(createUserGroup['request'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = true
+    })
+  )
+  .handleAction(createUserGroup['failure'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+    })
+  )
+  .handleAction(createUserGroup['success'], (state, payload) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+      draftState.userGroups.push(payload.payload.groupName)
+    })
+  )
+  .handleAction(getNetworkUsers['request'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = true
+    })
+  )
+  .handleAction(getNetworkUsers['success'], (state, payload) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+      draftState.networkUsers = payload.payload
+    })
+  )
+  .handleAction(getNetworkUsers['failure'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+    })
+  )
+  .handleAction(deleteNetworkUser['request'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = true
+    })
+  )
+  .handleAction(deleteNetworkUser['success'], (state, payload) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+      const { networkName, networkUserID } = payload.payload
+      draftState.networkUsers[networkName] = draftState.networkUsers[
+        networkName
+      ].filter((user) => user.id !== networkUserID)
+    })
+  )
+  .handleAction(deleteNetworkUser['failure'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+    })
+  )
+  .handleAction(updateNetworkUser['request'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = true
+    })
+  )
+  .handleAction(updateNetworkUser['success'], (state, payload) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+      // get the users of the network then update the user from the users
+      const { networkName, networkUser } = payload.payload
+      const users = draftState.networkUsers[networkName]
+      const userIndex = users.findIndex((user) => user.id === networkUser.id)
+      if (~userIndex) {
+        // if the user exists, update it
+        users[userIndex] = networkUser
+      }
+      draftState.networkUsers[networkName] = users
+    })
+  )
+  .handleAction(updateNetworkUser['failure'], (state, _) =>
+    produce(state, (draftState) => {
+      draftState.isProcessing = false
+    })
+  )
--- src/store/modules/pro/types.ts
+++ src/store/modules/pro/types.ts
@@ -1,13 +1,46 @@
-import { MutableRequired } from "../acls"
+import { MutableRequired } from '../acls'
 
-export type UserGroups =  MutableRequired<{ [groupName: string] : void}>
+export type UserGroups = MutableRequired<{ [groupName: string]: void }>
 
 export interface UserGroupsPayload {
-    Request: void
-    Response: UserGroups
+  Request: void
+  Response: UserGroups
 }
 
 export interface UserGroupDelorCreate {
-    Request: { groupName: string }
-    Response: { groupName: string }
+  Request: { groupName: string }
+  Response: { groupName: string }
+}
+
+export interface NetworkUser {
+  accesslevel: number
+  clientlimit: number
+  nodelimit: number
+  id: string
+  clients: Array<string>
+  nodes: Array<string>
+  groups: Array<string>
+}
+
+// export type NetworkUserMap = MutableRequired<{
+//   [networkUserID: string]: NetworkUser
+// }>
+
+export type NetworksUsersMap = MutableRequired<{
+  [networkName: string]: NetworkUser[]
+}>
+
+export interface NetworkUsersPayload {
+  Request: void
+  Response: NetworksUsersMap
+}
+
+export interface NetworkUserDelPayload {
+  Request: { networkName: string; networkUserID: string }
+  Response: { networkName: string; networkUserID: string }
+}
+
+export interface NetworkUserUpdatePayload {
+  Request: { networkName: string; networkUser: NetworkUser }
+  Response: { networkName: string; networkUser: NetworkUser }
 }
