524eb2ac3f2f7e5ac0b2dcb8d938dc15df2ef03c
0xdcarns <dillon.carns@gmail.com>
2022-08-12 13:44:33 -0400

    init

--- src/components/prodashboards/NodeUserDashboard.tsx
+++ src/components/prodashboards/NodeUserDashboard.tsx
@@ -1,20 +1,34 @@
 import { Container, Grid } from '@mui/material'
 import React from 'react'
-import { useRouteMatch, Switch, Route } from 'react-router-dom'
+import { useRouteMatch, Switch, Route, useParams } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { useLinkBreadcrumb } from '~components/PathBreadcrumbs'
 import NodeAccessCard from './components/NetAdminCards/NodeAccessCard'
 import ExtAccessCard from './components/NetAdminCards/ExtAccessCard'
 import { ExtClientViewVpn } from './components/vpnview/components/ExtClientViewVpn'
 import { NodeAccessView } from './components/NodeAccessView'
+import { useSelector } from 'react-redux'
+import { proSelectors } from '~store/selectors'
+import { Node } from '~store/types'
 
 export const NodeUserDashboard: React.FC = () => {
   const { path } = useRouteMatch()
   const { t } = useTranslation()
+  const { netid } = useParams<{ netid: string }>()
+  const netData = useSelector(proSelectors.networkUserData)[netid]
+  let nodes = [] as Node[]
+
+  if (!!netData) {
+    nodes = netData.nodes
+  }
 
   useLinkBreadcrumb({
     title: t('breadcrumbs.netadmindashboard'),
   })
+
+  if (!!nodes.length) {
+    return <div>not found</div>
+  }
 
   return (
     <Container>
